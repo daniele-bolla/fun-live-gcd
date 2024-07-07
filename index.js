6c657ad3545d53e48eb90aa21eae1b0bc2852e8b
bolladaniele <bolladaniele@gmail.com>
2024-07-07 14:24:35 +0100

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const functions = require('@google-cloud/functions-framework');
+
+// Register an HTTP function with the Functions Framework that will be executed
+// when you make an HTTP request to the deployed function's endpoint.
+functions.http('helloGET', (req, res) => {
+  res.send('Hello World!');
+});
+
+const docs = {
+  name: "gcd",
+  description: "Greater Common Divisor",
+  input: {
+    a: {
+      type: "number",
+      description: "First integer to compute gcd",
+      example: "78",
+    },
+    b: {
+      type: "number",
+      description: "Second integer to compute gcd",
+      example: "78",
+    },
+  },
+  output: {
+    type: "number",
+    description: "Greater Common Divisor of the two integer given",
+    example: "2",
+  },
+};
+
+function rgcd(a, b) {
+  if (b == 0) return a;
+  else return rgcd(b, a % b);
+}
+const gcd = async (req, res) => {
+  const { input } = req.body;
+  const { a, b } = input;
+  const output = rgcd(a, b);
+  res.send({ output });
+};
+
+app.post("/functions/gcd", gcd);
+app.get("/functions/gcd", (req, res) => {
+  res.json(docs);
+});
+
+exports.gcd = app;
